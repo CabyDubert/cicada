4b5f0f08b89093a088115b38a770daba6609cf4d
dubo <bo.du@oborcloud.com>
2020-09-15 19:42:21 +0800

    添加gateway

--- gateway/src/main/java/com/dubo/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/dubo/gateway/GatewayApplication.java
@@ -1,18 +1,15 @@
-package com.dubo.consumer;
+package com.dubo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-public class ConsumerApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
